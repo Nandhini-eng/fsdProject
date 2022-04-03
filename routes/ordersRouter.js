04ea3c2fcf2960120c08c25c0181c92b19d1bca4
Samhitha Reddy <samhithareddy905gmail.com>
2022-04-03 11:15:16 +0530

    added

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -7,6 +7,7 @@ const Orders = require('../models/orders');
 const ordersRouter = express.Router();
 
 ordersRouter.use(bodyParser.json());
+var cors= require('cors')
 
 
 ordersRouter.route('/')
@@ -34,14 +35,51 @@ ordersRouter.route('/')
     .catch((err) => next(err));
 })
 .delete((req, res, next) => {
-    Orders.remove({})
+    
+
+    res.statusCode = 403;
+    res.end('DELETE operation not supported on /orders');
+
+});
+
+
+
+
+
+ordersRouter.route('/:orderId')
+.options(cors(), (req,res) => {res.sendStatus(200); })
+.get((req,res,next) => {
+    Orders.findById(req.params.orderId)
+    .then((order) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(order);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post((req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /orders/'+ req.params.orderId);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+  res.end('PUT operation not supported on /orders/'+ req.params.orderId);
+    
+})
+.delete((req, res, next) => {
+    
+    Orders.findByIdAndRemove(req.params.orderId)
     .then((resp) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(resp);
     }, (err) => next(err))
-    .catch((err) => next(err));    
-});
+    .catch((err) => next(err));
+
+}
+
+);
+
 
 
 
