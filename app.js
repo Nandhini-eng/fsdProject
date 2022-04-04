0419a0b15add247deef380acf50529e16ae90a73
Nandhini-eng <56595669+Nandhini-eng@users.noreply.github.com>
2022-04-04 17:20:05 +0530

    Update app.js
    modified the url of the mongo cloud

--- app.js
+++ app.js
@@ -44,14 +44,9 @@ app.use(
 
 const mongoose = require('mongoose');
 
+//Provide your credentials for username,password to connect to the mongoDB Cloud
+const url = 'mongodb+srv://username:password@cluster0.4wv9m.mongodb.net/fsd3project';
 
-const url = 'mongodb+srv://bhagya:bhagya23@cluster0.4wv9m.mongodb.net/fsd3project';
-
-// const url = 'mongodb+srv://samhithareddy:Samhi_905@cluster0.4wv9m.mongodb.net/fsd3project';
-//const url = 'mongodb+srv://Bhanu:bhanu@cluster0.4wv9m.mongodb.net/fsd3project';
-//const url = 'mongodb+srv://Nandhini:Nandy2002@cluster0.4wv9m.mongodb.net/fsd3project';
-// const url = 'mongodb+srv://bhagya:bhagya23@cluster0.4wv9m.mongodb.net/fsd3project';
-// const url = 'mongodb+srv://samhithareddy:Samhi_905@cluster0.4wv9m.mongodb.net/fsd3project';
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
